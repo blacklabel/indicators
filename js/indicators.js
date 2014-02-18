1c66a66e8937832f777af2af6f5fece5585aa019
Sebastian Bochan <seba@nevoo.pl>
2014-02-18 15:27:39 +0100

    Tooltip formatter

--- js/indicators.js
+++ js/indicators.js
@@ -8,8 +8,8 @@
 		- getGraph(chart, series, options) 	- returns path, or columns as SVG elemnts to add.
 																					Doesn't add to chart via renderer! 
 		
-		***/
-		
+																					***/
+
 		/***
 		indicators: [{
 		    id: 'series-id',
@@ -29,25 +29,25 @@
 		
 		
 		var UNDEFINED,
-				Chart = HC.Chart,
-				Axis = HC.Axis,
-				extend = HC.extend,
-				each = HC.each,
-				merge = HC.merge;
+		Chart = HC.Chart,
+		Axis = HC.Axis,
+		extend = HC.extend,
+		each = HC.each,
+		merge = HC.merge;
 		
 		function error(name) {
-				if(window.console){
-						console.error(name);
-				}
+			if(window.console){
+				console.error(name);
+			}
 		}
 		
 		function forceRedraw(s){
-				if(s.indicators) {
-						each(s.indicators, function(el, i) {
-								el.isDirty = true;
-						});
-						s.indicators = null;
-				}
+			if(s.indicators) {
+				each(s.indicators, function(el, i) {
+					el.isDirty = true;
+				});
+				s.indicators = null;
+			}
 		}
 
 		HC.isArray = function(obj) {
@@ -56,6 +56,10 @@
 
 		HC.isObject = function(obj) {
 			return typeof obj === 'object';
+		}
+
+		HC.splat = function (obj) {
+			return HC.isArray(obj) ? obj : [obj];
 		}
 
 		
@@ -70,85 +74,126 @@
 		*  Remove corresponding indicators for series
 		*/
 		HC.wrap(HC.Series.prototype, 'update', function(proceed, redraw, animation) {
-				var tempIndics = [],
-						s = this;
-						
-				if(s.indicators) {
-						each(s.indicators, function(el, i) {
-								tempIndics.push(merge({}, el.options));
-						});
-				}
-				proceed.call(this, redraw, animation);
-				
-				s = this;
-				each(tempIndics, function(el, i){
-						s.chart.addIndicator(el);
-				});
-		});
-		
+			var tempIndics = [],
+			s = this;
 
+			if(s.indicators) {
+				each(s.indicators, function(el, i) {
+					tempIndics.push(merge({}, el.options));
+				});
+			}
+			proceed.call(this, redraw, animation);
+
+			s = this;
+			each(tempIndics, function(el, i){
+				s.chart.addIndicator(el);
+			});
+		});
 		
 		/*
 		*  Remove corresponding indicators for series
 		*/
 		HC.wrap(HC.Series.prototype, 'remove', function(proceed, redraw, animation) {
-				var s = this;
-				if(s.indicators) {
-						each(s.indicators, function(el, i) {
-								el.destroy();
-						});
-						s.indicators = null;
-				}
-				proceed.call(this, redraw, animation);
+			var s = this;
+			if(s.indicators) {
+				each(s.indicators, function(el, i) {
+					el.destroy();
+				});
+				s.indicators = null;
+			}
+			proceed.call(this, redraw, animation);
 		});
 		
 		/*
 		*  Force redraw for indicator with new data
 		*/
 		HC.wrap(HC.Series.prototype, 'setData', function(proceed, redraw, animation) {
-				forceRedraw(this);
-				proceed.call(this, redraw, animation);
+			forceRedraw(this);
+			proceed.call(this, redraw, animation);
 		});
 		
 		/*
 		*  Force redraw for indicator when new point is added
 		*/
 		HC.wrap(HC.Series.prototype, 'addPoint', function(proceed, options, redraw, shift, animation) {
-				forceRedraw(this);
-				proceed.call(this, options, redraw, shift, animation);
+			forceRedraw(this);
+			proceed.call(this, options, redraw, shift, animation);
 		});
 		
 		/*
 		*  Force redraw for indicator with new point options, like value
 		*/
 		HC.wrap(HC.Point.prototype, 'update', function(proceed, options, redraw) {
-				forceRedraw(this.series);
-				proceed.call(this, options, redraw);
+			forceRedraw(this.series);
+			proceed.call(this, options, redraw);
 		});
 		
 		/*
 		*  Force redraw for indicator when one of points is removed
 		*/
 		HC.wrap(HC.Point.prototype, 'remove', function(proceed, redraw, animation) {
-				forceRedraw(this.series);
-				proceed.call(this, options, redraw);
+			forceRedraw(this.series);
+			proceed.call(this, options, redraw);
 		});
 		
 
 		/*
 		*		Set flag for navigator yAxis
 		*/
-    HC.wrap(HC.Scroller.prototype, 'init', function (proceed) {
-    	proceed.call(this);
-      this.yAxis.isNavigator = true;
-    });
+		HC.wrap(HC.Scroller.prototype, 'init', function (proceed) {
+			proceed.call(this);
+			this.yAxis.isNavigator = true;
+		});
+
+    /*
+		*		Tooltip formatter content
+		*/
+
+		HC.wrap(HC.Tooltip.prototype, 'defaultFormatter',function (proceed, options, redraw) {
+
+			var points 		 = this.points || HC.splat(this),
+					series 		 = points[0].series,
+					chart 		 = series.chart,
+					tooltipOptions = chart.tooltip.options,
+					indicators = series.indicators,
+					x 			   = this.x,
+					s;
+
+			// build the header
+			s = [series.tooltipHeaderFormatter(points[0])];
+
+			// build the values
+			each(points, function (item) {
+					series = item.series;
+					s.push((series.tooltipFormatter && series.tooltipFormatter(item)) ||
+					item.point.tooltipFormatter(series.tooltipOptions.pointFormat));
+			});
+
+			if(tooltipOptions.enabledIndicators) {
+				// build the values of indicators
+				$.each(indicators,function(i,ind){
+					if(typeof(ind.values) === 'undefined') 
+						return;
+
+					$.each(ind.values,function(j,val){
+						if(val[0] === x) 
+							s.push(ind.options.type.toLowerCase() + ': ' + HC.numberFormat(val[1],2) + '<br/>');
+					});
+				});
+			}
+
+			// build the footer
+			s.push(tooltipOptions.footerFormat || '');
+			return s.join('');
+
+		});
 
 		/***
 		
 		Indicator Class:
 		
 		***/
-				
+
 		Indicator = Indicator = function () {
 			this.init.apply(this, arguments);
 		};
@@ -166,7 +211,7 @@
 				this.options = options;
 				this.series = chart.get(options.id);
 				if(!this.series.indicators) {
-						this.series.indicators = [];
+					this.series.indicators = [];
 				}
 				this.series.indicators.push(this);
 			},
@@ -176,104 +221,55 @@
 			*/
 			render: function (redraw) {
 				var indicator = this,
-						chart = this.chart,
-						renderer = chart.renderer,
-						graph = this.graph,
-						group = this.group,
-						options = this.options,
-						series = this.series,
-						arrayValues,
-						extremes;
-						
+				chart = this.chart,
+				renderer = chart.renderer,
+				group = this.group,
+				graph = this.graph,
+				options = this.options,
+				series = this.series;
+
 				if (!group) {
-						indicator.group = group = renderer.g().add();
-						indicator.group.clip(chart.indicators.clipPath);
+					indicator.group = group = renderer.g().add();
 				}
+				
 				if(!series) {
-						error('Series not found');
-						return false;
+					error('Series not found');
+					return false;
 				} else if(!graph) {
-						arrayValues = Indicator[options.type].getValues(chart, series, options);
-						if(arrayValues) {
-							this.values = arrayValues.values;
-							this.xData = arrayValues.xData;
-							this.yData = arrayValues.yData;
-							extremes = this.getXExtremes();
-							this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values.slice(extremes.min, extremes.max));
-							graph.add(group);
-						}
+					this.values = Indicator[options.type].getValues(chart, series, options);
+					if(this.values) {
+						this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values);
+						graph.add(group);
+					}
 				}
 			},
-
+			
 			/*
 			* Redraw the indicator 
 			*/
 			redraw: function () {
 				var options = this.options,
-						chart = this.chart,
-						series = this.series,
-						graph = this.graph,
-						group = this.group,
-						isDirty = this.isDirty,
-						arrayValues,
-						extremes;
-						
+				chart = this.chart,
+				series = this.series,
+				group = this.group,
+				graph = this.graph,
+				isDirty = this.isDirty;
+
 				if(graph) {
-						graph.destroy();
+					graph.destroy();
 				}
 				
 				if(this.values && !isDirty) {
-						extremes = this.getXExtremes();
-						this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values.slice(extremes.min, extremes.max));
-						// shouldn't be attr({ d: path }); ?
-						graph.add(group);
+					this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values);
+					graph.add();
 				} else {
-						arrayValues = Indicator[options.type].getValues(chart, series, options);
-						if(arrayValues) {
-							this.values = arrayValues.values;
-							this.xData = arrayValues.xData;
-							this.yData = arrayValues.yData;
-							extremes = this.getXExtremes();
-							this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values.slice(extremes.min, extremes.max));
-							graph.add(group);
-						}
-				}
-			},	
-			
-			/*
-			* Get x-extremes for indicator to draw path only within plotting area
-			*/
-			getXExtremes: function(){
-				var xData = this.xData,
-						xAxis = this.xAxis || this.series.xAxis,
-						len = xData.length,
-						xMin = xAxis.min,
-						xMax = xAxis.max,
-						i = 0,
-						min,
-						max;
-				
-				while(i < len){
-					if(xData[i] >= xMin) {
-						min = i;
-						i = len;
+					this.values = Indicator[options.type].getValues(chart, series, options);
+					if(this.values) {
+						this.graph = graph = Indicator[options.type].getGraph(chart, series, options, this.values);
+						graph.add();
 					}
-					i++;
 				}
-				while(i > 0){
-					if(xData[i] <= xMax) {
-						max = i;
-						i = 0;
-					}
-					i--;
-				}
-				min = min < 3 ? 0 : min - 2;
-				max = max > len - 4 ? len : max + 3;
 				
-				return {
-						min: min,
-						max: max
-				}
 			},
 			
 			/*
@@ -281,9 +277,9 @@
 			*/
 			destroy: function () {
 				var indicator = this,
-						chart = this.chart,
-						allItems = chart.indicators.allItems,
-						index = allItems.indexOf(indicator);
+				chart = this.chart,
+				allItems = chart.indicators.allItems,
+				index = allItems.indexOf(indicator);
 				
 				if (index > -1) {
 					allItems.splice(index, 1);
@@ -310,19 +306,19 @@
 			* Hide the indicator
 			*/
 			hide: function() {
-					this.group.hide();
+				this.group.hide();
 			},
 			
 			/*
 			* Show the indicator
 			*/
 			show: function() {
-					this.group.show();
+				this.group.show();
 			}
 		};
 		
 		
-	
+
 		// Add indicator methods to chart prototype
 		extend(Chart.prototype, {
 				/*
@@ -330,8 +326,8 @@
 				*/
 				addIndicator: function (options, redraw) {
 					var chart = this,
-							indicators = chart.indicators.allItems,
-							item;
+					indicators = chart.indicators.allItems,
+					item;
 					
 					item = new Indicator(chart, options);
 					indicators.push(item);
@@ -340,80 +336,80 @@
 				/*
 				 * Redraw all indicators, method used in chart events
 				 */
-				redrawIndicators: function () {
-						each(this.indicators.allItems, function (indicator) {
-									indicator.redraw();
-						});
-				}
-		});
+				 redrawIndicators: function () {
+				 	each(this.indicators.allItems, function (indicator) {
+				 		indicator.redraw();
+				 	});
+				 }
+				});
 		
 		// Add yAxis as pane
 		extend(Axis.prototype, {
 			addAxisPane: function(chart, userOptions) {
-            var defaultOptions = {
-            	  title:{
-            	  	text:'ATR'
-            	  },
-            	  labels:{
-            	  	x: -8,
-									y: -2
-            	  },
-                offset: -25,
-                height: 250,
-                top: 0,
-                min: 0,
-                max: 100
-            }
-            
-            var chYxis = chart.yAxis,
-                len = chYxis.length,
-                top = chYxis[0].top,
-                topDiff = len > 2 ? chYxis[1].top - chYxis[0].top - chYxis[0].options.height : top,
-                options = merge(defaultOptions,userOptions),
-                i = sum = 0,
-                hp = [],
-                lastTop;
-            
+				var defaultOptions = {
+					title:{
+						text:'ATR'
+					},
+					labels:{
+						x: -8,
+						y: -2
+					},
+					offset: -25,
+					height: 250,
+					top: 0,
+					min: 0,
+					max: 100
+				}
+
+				var chYxis = chart.yAxis,
+				len = chYxis.length,
+				top = chYxis[0].top,
+				topDiff = len > 2 ? chYxis[1].top - chYxis[0].top - chYxis[0].options.height : top,
+				options = merge(defaultOptions,userOptions),
+				i = sum = 0,
+				hp = [],
+				lastTop;
+
             //calculate height
             for (i = 0; i < len; i++) {
-                if(!chYxis[i].isNavigator) 
-                     sum += chYxis[i].height;
+            	if(!chYxis[i].isNavigator) 
+            		sum += chYxis[i].height;
             };
             
             //update all axis
             for (i = 0; i < len; i++) {
-                var yAxis = chYxis[i];
-                
-                if(!yAxis.isNavigator) {
+            	var yAxis = chYxis[i];
 
-                    var yAxisHeight = yAxis.height,
-                        paneHeight = options.height,
-                        hPercent = paneHeight / sum,
-                        diffHeight, newHeight;
-                    
-                    if(hPercent > 0.5) {
-                    	paneHeight = (yAxisHeight / 2);
-                    	hPercent = paneHeight / sum;
-                    }
+            	if(!yAxis.isNavigator) {
 
-                    diffHeight = sum * hPercent * (yAxisHeight / sum), 
-                    newHeight = yAxisHeight - diffHeight - (topDiff / (len - 1));
-                    options.height = paneHeight;
+            		var yAxisHeight = yAxis.height,
+            		paneHeight = options.height,
+            		hPercent = paneHeight / sum,
+            		diffHeight, newHeight;
 
-                    if(i > 0) {
-                        var prevP = chYxis[i-1].isNavigator ? chYxis[i-2] : chYxis[i-1], 
-                            prevH = prevP.options.height,
-                            prevOldH = prevP.height;
-                        
-                        top = prevP.options.top + prevH + topDiff;
-                    } 
-                    
-                    lastTop = top + newHeight + topDiff;
-                    chYxis[i].update({
-                        top: top,
-                        height: newHeight
-                    },false);
-                }
+            		if(hPercent > 0.5) {
+            			paneHeight = (yAxisHeight / 2);
+            			hPercent = paneHeight / sum;
+            		}
+
+            		diffHeight = sum * hPercent * (yAxisHeight / sum), 
+            		newHeight = yAxisHeight - diffHeight - (topDiff / (len - 1));
+            		options.height = paneHeight;
+
+            		if(i > 0) {
+            			var prevP = chYxis[i-1].isNavigator ? chYxis[i-2] : chYxis[i-1], 
+            			prevH = prevP.options.height,
+            			prevOldH = prevP.height;
+
+            			top = prevP.options.top + prevH + topDiff;
+            		} 
+
+            		lastTop = top + newHeight + topDiff;
+            		chYxis[i].update({
+            			top: top,
+            			height: newHeight
+            		},false);
+            	}
             }
             
             //add new axis
@@ -421,42 +417,42 @@
             chart.addAxis(options);
 
             return (chart.yAxis.length - 1);
-			},
-			minInArray: function(arr) {
-				return arr.reduce(function (p, v) {
-			    return ( p < v ? p : v );
-			  })[1];
-			},
-			maxInArray: function(arr) {
-				return arr.reduce(function (p, v) {
-			    return ( p > v ? p : v );
-			  })[1];
-			}
-		});
-		
+          },
+          minInArray: function(arr) {
+          	return arr.reduce(function (p, v) {
+          		return ( p < v ? p : v );
+          	})[1];
+          },
+          maxInArray: function(arr) {
+          	return arr.reduce(function (p, v) {
+          		return ( p > v ? p : v );
+          	})[1];
+          }
+        });
+
 		// Initialize on chart load
 		Chart.prototype.callbacks.push(function (chart) {
-        var options = chart.options.indicators,
-        		optionsLen = options ? options.length : 0,
-        		clipPath,
-						group,
-						clipBox = {
-								x: chart.plotLeft,
-								y: chart.plotTop,
-								width: chart.plotWidth,
-								height: chart.plotHeight
-						};
-        
-        clipPath = chart.renderer.clipRect(clipBox);   
-        group = chart.renderer.g("indicators");
-        group.attr({
-        		zIndex: 7
-        });
-        group.clip(clipPath);
-        group.add();
-        
-        if(!chart.indicators) chart.indicators = {};
-        
+			var options = chart.options.indicators,
+			optionsLen = options ? options.length : 0,
+			clipPath,
+			group,
+			clipBox = {
+				x: chart.plotLeft,
+				y: chart.plotTop,
+				width: chart.plotWidth,
+				height: chart.plotHeight
+			};
+
+			clipPath = chart.renderer.clipRect(clipBox);   
+			group = chart.renderer.g("indicators");
+			group.attr({
+				zIndex: 7
+			});
+			group.add();
+			group.clip(clipPath);
+
+			if(!chart.indicators) chart.indicators = {};
+
         // initialize empty array for indicators
         if(!chart.indicators.allItems) chart.indicators.allItems = [];
         
@@ -470,16 +466,16 @@
         // add clip path to indicators
         chart.indicators.clipPath = clipPath;
         
-       for(var i = 0; i < optionsLen; i++) {
-        		chart.addIndicator(options[i]);
-       }
+        for(var i = 0; i < optionsLen; i++) {
+        	chart.addIndicator(options[i]);
+        }
         
 				// update indicators after chart redraw
-			 chart.redrawIndicators();
-			 Highcharts.addEvent(chart, 'redraw', function () {
+				chart.redrawIndicators();
+				Highcharts.addEvent(chart, 'redraw', function () {
 					chart.redrawIndicators();
-			 });
-		});
+				});
+			});
 
 
 })(Highcharts);
