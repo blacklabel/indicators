6ca05fc13f7d7bc760d4b4631f9a4ef0f733d7f3
Paweł Fus <pawel.fus.88@gmail.com>
2016-06-02 17:05:34 +0200

    Linted indicators, close #41.

--- js/demo.js
+++ js/demo.js
@@ -1,105 +1,106 @@
+/* global $:true */
 $(function () {
 	/*
 	* 2.1 ADVANCED CHART
 	*/
-	var adv_options = {
-			indicators: [{
-					id: 'AAPL',
-					type: 'sma',
-					name: 'SMA',
-					tooltip:{
-							pointFormat: '<span style="color: {point.color}; ">SMA value: </span> {point.y}<br>'
-					},
-					params: {
-							period: 14
-					},
-					showInLegend: true
-			}, {
-					id: 'AAPL',
-					type: 'ema',
-					params: {
-							period: 14,
-							index: 0 //optional parameter for ohlc / candlestick / arearange - index of value
-					},
-					styles: {
-							strokeWidth: 2,
-							stroke: 'green',
-							dashstyle: 'solid'
-					},
-					showInLegend: true
-			},{
-					id: 'AAPL',
-					type: 'atr',
-					params: {
-							period: 14
-					},
-					styles: {
-							strokeWidth: 2,
-							stroke: 'orange',
-							dashstyle: 'solid'
-					},
-					yAxis: {
-							lineWidth: 2,
-							title: {
-									text:'ATR'
-							}
-					},
-					showInLegend: true
-			}, {
-					id: 'AAPL',
-					type: 'rsi',
-					params: {
-							period: 14,
-							overbought: 70,
-							oversold: 30
-					},
-					styles: {
-							strokeWidth: 2,
-							stroke: 'black',
-							dashstyle: 'solid'
-					},
-					yAxis: {
-							lineWidth: 2,
-							title: {
-									text:'RSI'
-							}
-					},
-					showInLegend: true
-			}],
-			yAxis:{
-					opposite: false,
-					title: {
-							text: 'DATA SMA EMA',
-							x: -4
-					},
-					lineWidth: 2,
-					labels:{
-							x: 22
-					}
+	var advOptions = {
+		indicators: [{
+			id: 'AAPL',
+			type: 'sma',
+			name: 'SMA',
+			tooltip: {
+				pointFormat: '<span style="color: {point.color}; ">SMA value: </span> {point.y}<br>'
 			},
-			rangeSelector: {
-					selected: 0
+			params: {
+				period: 14
 			},
-			tooltip:{
-					enabledIndicators: true
+			showInLegend: true
+		}, {
+			id: 'AAPL',
+			type: 'ema',
+			params: {
+				period: 14,
+				index: 0 // optional parameter for ohlc / candlestick / arearange - index of value
 			},
-			series: [{
-					cropThreshold: 0,
-					id: 'AAPL',
-					name: 'AAPL',
-					data: [],
-					tooltip: {
-							valueDecimals: 2
-					}
-			}]
+			styles: {
+				strokeWidth: 2,
+				stroke: 'green',
+				dashstyle: 'solid'
+			},
+			showInLegend: true
+		}, {
+			id: 'AAPL',
+			type: 'atr',
+			params: {
+				period: 14
+			},
+			styles: {
+				strokeWidth: 2,
+				stroke: 'orange',
+				dashstyle: 'solid'
+			},
+			yAxis: {
+				lineWidth: 2,
+				title: {
+					text: 'ATR'
+				}
+			},
+			showInLegend: true
+		}, {
+			id: 'AAPL',
+			type: 'rsi',
+			params: {
+				period: 14,
+				overbought: 70,
+				oversold: 30
+			},
+			styles: {
+				strokeWidth: 2,
+				stroke: 'black',
+				dashstyle: 'solid'
+			},
+			yAxis: {
+				lineWidth: 2,
+				title: {
+					text: 'RSI'
+				}
+			},
+			showInLegend: true
+		}],
+		yAxis: {
+			opposite: false,
+			title: {
+				text: 'DATA SMA EMA',
+				x: -4
+			},
+			lineWidth: 2,
+			labels: {
+				x: 22
+			}
+		},
+		rangeSelector: {
+			selected: 0
+		},
+		tooltip: {
+			enabledIndicators: true
+		},
+		series: [{
+			cropThreshold: 0,
+			id: 'AAPL',
+			name: 'AAPL',
+			data: [],
+			tooltip: {
+				valueDecimals: 2
+			}
+		}]
 	};
 
-	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function(data) {
-	//$.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
+	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json&callback=?', function (data) {
+	// $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
 	
-			adv_options.series[0].type = 'candlestick';
-			adv_options.series[0].data = data;
-	
-			$('#container-advanced').highcharts('StockChart', adv_options);
+		advOptions.series[0].type = 'candlestick';
+		advOptions.series[0].data = data;
+
+		$('#container-advanced').highcharts('StockChart', advOptions);
 	});
 });
--- js/ema.js
+++ js/ema.js
@@ -1,162 +1,145 @@
-(function (HC) {
-		/***
-    
-    Each indicator requires mothods:
-    
-    - getDefaultOptions()               				- returns object with default parameters, like period etc.
-    - getValues(chart, series, options, points) - returns array of calculated values for indicator
-    - getGraph(chart, series, options, values)  - returns path, or columns as SVG elements to add.
-                                         				  Doesn't add to the chart via renderer! 
-    
-    ***/
-    
-    /***
-    indicators: [{
-        id: 'series-id',
-        type: 'ema',
-        params: {
-            period: 'x',
-            index: 0
-        },    
-        styles: {
-            lineWidth: 'x',
-            strokeColor: 'y'
-        }
-    }]
-    
-    ***/
+(function (HC, Indicator) {
+	/* global Highcharts Indicator:true */
+	'use strict';
+	/**
+	
+	Each indicator requires mothods:
+	
+	- getDefaultOptions()               		- returns object with default parameters, like period etc.
+	- getValues(chart, series, options, points) - returns array of calculated values for indicator
+	- getGraph(chart, series, options, values)  - returns path, or columns as SVG elements to add.
+													Doesn't add to the chart via renderer!
+	
+	**/
 
-    var merge   = HC.merge,
-        isArray = HC.isArray,
-        minInArray = HC.Axis.prototype.minInArray,
-        maxInArray = HC.Axis.prototype.maxInArray,
-        UNDEFINED;
-    
-    Indicator.prototype.ema = {
-        getDefaultOptions: function(){
-            return {
-                period: 14,
-                index: 0,
-								approximation: "average"
-            };
-        },
-        getValues: function(chart, series, options, extraPoints) {
-            var utils = this.utils,
-                params = options.params,
-                period = params.period,
-                xVal = extraPoints[0].concat(series.processedXData || []), // #22
-                yVal = extraPoints[1].concat(series.processedYData || []), // #22
-                yValLen = yVal ? yVal.length : 0,
-                EMApercent = (2 / (period + 1)),
-                calEMA = 0,
-                range = 1,
-                xValue = xVal[0],
-                yValue = yVal[0],
-                EMA = [],
-                xData = [],
-                yData = [],
-                index = -1,
-                point,i,points,
-                EMAPoint;
+	var merge = HC.merge,
+		isArray = HC.isArray,
+		minInArray = HC.Axis.prototype.minInArray,
+		maxInArray = HC.Axis.prototype.maxInArray;
+	
+	Indicator.prototype.ema = {
+		getDefaultOptions: function () {
+			return {
+				period: 14,
+				index: 0,
+				approximation: 'average'
+			};
+		},
+		getValues: function (chart, series, options, extraPoints) {
+			var utils = this.utils,
+				params = options.params,
+				period = params.period,
+				xVal = extraPoints[0].concat(series.processedXData || []), // #22
+				yVal = extraPoints[1].concat(series.processedYData || []), // #22
+				yValLen = yVal ? yVal.length : 0,
+				EMApercent = (2 / (period + 1)),
+				calEMA = 0,
+				range = 1,
+				xValue = xVal[0],
+				yValue = yVal[0],
+				EMA = [],
+				xData = [],
+				yData = [],
+				index = -1,
+				i, points,
+				EMAPoint;
 
-           //check period, if bigger than points length, skip
-           if(xVal.length <= period) {
-            return;
-           }
+			// check period, if bigger than points length, skip
+			if (xVal.length <= period) {
+				return false;
+			}
 
+			// switch index for OHLC / Candlestick / Arearange
+			if (isArray(yVal[0])) {
+				index = params.index ? params.index : 0;
+				yValue = yVal[0][index];
+			}
 
-           //switch index for OHLC / Candlestick / Arearange
-           if(isArray(yVal[0])) {
-              index = params.index ? params.index : 0;
-              yValue = yVal[0][index];
-           }
-           // starting point
-           points = [[xValue, yValue]];
-           
-           // accumulate first N-points
-           while(range != period){
-               utils.accumulateAverage(points, xVal, yVal, range, index); 
-               range ++; 
-           }
-           // calculate value one-by-one for each perdio in visible data
-           for(i = range; i < yValLen; i++){
-									EMAPoint = utils.populateAverage(points, xVal, yVal, i, EMApercent, calEMA, index);
-									EMA.push(EMAPoint);
-									xData.push(EMAPoint[0]);
-									yData.push(EMAPoint[1]);
-                  calEMA = EMAPoint[1]; 
+			// starting point
+			points = [[xValue, yValue]];
 
-                  utils.accumulateAverage(points, xVal, yVal, i, index);   
-           }
-           
-					 EMAPoint = utils.populateAverage(points, xVal, yVal, i, EMApercent, calEMA, index);
-					 EMA.push(EMAPoint);
-					 xData.push(EMAPoint[0]);
-					 yData.push(EMAPoint[1]);
+			// accumulate first N-points
+			while (range !== period) {
+				utils.accumulateAverage(points, xVal, yVal, range, index);
+				range++;
+			}
 
-            // registger extremes for axis;
-					 options.yAxisMax = maxInArray(EMA);
-					 options.yAxisMin = minInArray(EMA);
-					 
-           return {
-           	 values: EMA,
-           	 xData: xData,
-           	 yData: yData
-           };
-           
-        }, 
-        getGraph: function(chart, series, options, values) {
-           var path   = [],
-               attrs  = {},
-               xAxis  = series.xAxis,
-               yAxis  = options.Axis = series.yAxis,
-               ema    = values,
-               emaLen = ema.length,
-               emaX,
-               emaY,
-               i;
+			// calculate value one-by-one for each perdio in visible data
+			for (i = range; i < yValLen; i++) {
+				EMAPoint = utils.populateAverage(points, xVal, yVal, i, EMApercent, calEMA, index);
+				EMA.push(EMAPoint);
+				xData.push(EMAPoint[0]);
+				yData.push(EMAPoint[1]);
+				calEMA = EMAPoint[1];
 
-            if(options.visible === false) {
-              return;
-           }
-               
-           options.styles = attrs = merge({
-               'stroke-width': 2,
-               stroke: 'red',
-               dashstyle: 'Dash'
-           },  options.styles);  
-           
-           path.push('M', xAxis.toPixels(ema[0][0]), yAxis.toPixels(ema[0][1])); 
-               
-           for(i = 0; i < emaLen; i++){
-              emaX = ema[i][0];
-              emaY = ema[i][1];
-              
-              path.push('L', xAxis.toPixels(emaX), yAxis.toPixels(emaY));
-           }
+				utils.accumulateAverage(points, xVal, yVal, i, index);
+			}
 
-           return [chart.renderer.path(path).attr(attrs)];
-        },
-        utils: {
-            accumulateAverage: function(points, xVal, yVal, i, index){ 
+			EMAPoint = utils.populateAverage(points, xVal, yVal, i, EMApercent, calEMA, index);
+			EMA.push(EMAPoint);
+			xData.push(EMAPoint[0]);
+			yData.push(EMAPoint[1]);
 
-                var xValue = xVal[i],
-                    yValue = index < 0 ? yVal[i] : yVal[i][index];
-                    
-                points.push([xValue, yValue]);
-            },
-            populateAverage: function(points, xVal, yVal, i, EMApercent, calEMA, index){
-                var pLen       = points.length,
-                    x          = xVal[i-1],
-                    yValuePrev = index < 0 ? yVal[i-2] : yVal[i-2][index],
-                    yValue     = index < 0 ? yVal[i-1] : yVal[i-1][index],
-                    prevPoint,y;
+			// registger extremes for axis;
+			options.yAxisMax = maxInArray(EMA);
+			options.yAxisMin = minInArray(EMA);
 
-                prevPoint = calEMA === 0 ? yValuePrev : calEMA;
-                y = ((yValue * EMApercent) + (prevPoint * (1 - EMApercent)));
+			return {
+				values: EMA,
+				xData: xData,
+				yData: yData
+			};
+		},
+		getGraph: function (chart, series, options, values) {
+			var path = [],
+				attrs = {},
+				xAxis = series.xAxis,
+				yAxis = options.Axis = series.yAxis,
+				ema = values,
+				emaLen = ema.length,
+				emaX,
+				emaY,
+				i;
 
-                return [x, y];
-            }
-        }
-    };
-})(Highcharts);
+			if (options.visible === false) {
+				return false;
+			}
+
+			options.styles = attrs = merge({
+				'stroke-width': 2,
+				stroke: 'red',
+				dashstyle: 'Dash'
+			}, options.styles);
+
+			path.push('M', xAxis.toPixels(ema[0][0]), yAxis.toPixels(ema[0][1]));
+
+			for (i = 0; i < emaLen; i++) {
+				emaX = ema[i][0];
+				emaY = ema[i][1];
+
+				path.push('L', xAxis.toPixels(emaX), yAxis.toPixels(emaY));
+			}
+
+			return [chart.renderer.path(path).attr(attrs)];
+		},
+		utils: {
+			accumulateAverage: function (points, xVal, yVal, i, index) {
+				var xValue = xVal[i],
+					yValue = index < 0 ? yVal[i] : yVal[i][index];
+					
+				points.push([xValue, yValue]);
+			},
+			populateAverage: function (points, xVal, yVal, i, EMApercent, calEMA, index) {
+				var x = xVal[i - 1],
+					yValuePrev = index < 0 ? yVal[i - 2] : yVal[i - 2][index],
+					yValue = index < 0 ? yVal[i - 1] : yVal[i - 1][index],
+					prevPoint, y;
+
+				prevPoint = calEMA === 0 ? yValuePrev : calEMA;
+				y = ((yValue * EMApercent) + (prevPoint * (1 - EMApercent)));
+
+				return [x, y];
+			}
+		}
+	};
+})(Highcharts, Indicator);
