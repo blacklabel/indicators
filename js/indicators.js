b1399bbe69be64c1f3bd952b11021b556dfedee5
Phil Chen <clw.air@gmail.com>
2016-07-29 16:15:24 +1000

    Indicators cannot display if series.type is 'line'
    Indicators cannot display if series.type is 'line / kagi / spline / area / area range / area spline range'.
    In normal condition every element in the array 'series.yData' should be another array with 4 values. But in these cases above ( e.g. when the type of series is line ) the array 'series.yData' is only an array whose elements are all plain numbers ( see official API : series.data doesn't contain all the points. It only contains the points that have been created on demand......Additionally, 'series.options.data' contains all configuration objects for the points ). So addition function properYData(series) is added utilizing the variable 'series.options.data' to deal with this problem and return the correct yData array.

--- js/indicators.js
+++ js/indicators.js
@@ -79,6 +79,20 @@
 			followPointer: true
 		}
 	});
+	
+	// Pass correct series.yData to getValues() before render()
+	function properYData(series) {
+		if (HC.isArray(series.yData) && (series.yData.length >0) && HC.isArray(series.yData[0]) && (series.yData[0].length === 4)) {
+			return series.yData;
+		}
+		else {
+			var arr = [];
+			series.options.data.forEach(function(value) {
+				arr.push([value[0], value[1], value[2], value[3]])
+			});
+			return arr;
+		}
+	}
 
 	
 	/**
@@ -519,7 +533,7 @@
 				error('Series not found');
 				return;
 			} else if (!graph) {
-				arrayValues = Indicator.prototype[options.type].getValues(chart, { points: [] }, options, [series.xData, series.yData]);
+				arrayValues = Indicator.prototype[options.type].getValues(chart, { points: [] }, options, [series.xData, properYData(series)]);
 				if (!arrayValues) { // #6 - create dummy data
 					arrayValues = {
 						values: [[]],
